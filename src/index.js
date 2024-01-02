5be0bfca7f476e9fc1c819e12269d811b5fed184
Vedank V Vekhande <vedank98@gmail.com>
2024-01-02 18:03:25 +0530

    updated index file

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {BrowserRouter,Routes,Route} from 'react-router-dom'
 // import './index.css';
-import App from './App';
-import Home from './todo/Home';
-import Courosal from './courosal/Corousal'
+// import App from './App';
+// import Home from './todo/Home';
+// import Courosal from './courosal/Corousal'
 // import Tictactoe from './tictactoe/Tictactoe'
 import reportWebVitals from './reportWebVitals';
 // import Navbar from './E-Commerce/Components/Navbar';
